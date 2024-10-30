0f61579115b7a92ca7ac83c5bfbeec69246dd16a
Samuel Rincon <sam_rncn@gmail.com>
2024-10-30 06:45:33 -0700

    fix need corrections, 2nd iteration

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,6 @@ const initialCards = [
   {name:"Happy Hour", link: "https://images.unsplash.com/photo-1728177196109-f6d1227c3fb9?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", alt: "fridge with clear glass door filled with wine"},
   {name:"Horse with no name", link: "https://images.unsplash.com/photo-1534774474501-25b647a3f1b2?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", alt: "ash grey horse walking on mountan side"},
   {name:"Glendale", link: "https://images.unsplash.com/photo-1528709024086-98a7672e0b9d?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", alt: "Sheep in a field"},
-  {name:"praticum", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg", alt: "Sheep in a field"}
 ];
 // used for edit profile modal
 const profileEditButton = document.querySelector(".profile__edit-btn");
@@ -25,8 +24,8 @@ const cardsList = document.querySelector(".cards__list");
 
 // used for add-card-modal
 const addCardModal = document.querySelector("#add-card-modal");
-const newPostbuttton = document.querySelector(".profile__add-btn");
-const closePostbutton = addCardModal.querySelector(".modal__close-btn");
+const newPostButtton = document.querySelector(".profile__add-btn");
+const closePostButton = addCardModal.querySelector(".modal__close-btn");
 const cardForm = addCardModal.querySelector(".modal__form");
 const cardLinkInput = addCardModal.querySelector("#add-card-link-input");
 const cardCaptionInput = addCardModal.querySelector("#add-card-name-input");
@@ -58,10 +57,6 @@ function getCardElement (data) {
     previewImgModal.alt = data.alt;
     previewCaptionModal.textContent = data.name;
   })
-
-  previewCloseModalBtn.addEventListener("click", () => {
-    closeModal(previewModal);
-  });
 
 
   cardNameElement.textContent = data.name;
@@ -95,6 +90,7 @@ function handleCardSubmit (evt){
   const cardsElement = getCardElement(inputValue);
   cardsList.prepend(cardsElement);
   closeModal(addCardModal);
+  evt.target.reset();
 
 };
 
@@ -105,20 +101,22 @@ profileEditButton.addEventListener("click", () => {
 });
 
 
-
 closeProfileModal.addEventListener("click", () => {
   closeModal(editProfileModal );
 
 });
 
-newPostbuttton.addEventListener("click", () => {
+newPostButtton.addEventListener("click", () => {
   openModal(addCardModal);
 });
 
-closePostbutton.addEventListener("click", () =>{
+closePostButton.addEventListener("click", () =>{
   closeModal(addCardModal);
 })
 
+previewCloseModalBtn.addEventListener("click", () => {
+  closeModal(previewModal);
+});
 
 
 editFormElement.addEventListener("submit", handleEditFormSubmit);
